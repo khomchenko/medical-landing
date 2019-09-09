758bb1655b70dbf768de9f131ad84f603dd79b0b
Valera Khomchenko <valerakhomchenko@gmail.com>
2019-09-09 17:25:12 +0300

    Add gallery and blog section

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -7,4 +7,26 @@ var membersSwiper = new Swiper ('.members__wrap .swiper-container', {
         nextEl: '.members__wrap .swiper-button-next',
         prevEl: '.members__wrap .swiper-button-prev',
     }
-})
+});
+
+var gallerySwiper = new Swiper ('.gallery__wrap .swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.gallery__wrap .swiper-button-next',
+        prevEl: '.gallery__wrap .swiper-button-prev',
+    }
+});
+
+var blogSwiper = new Swiper ('.blog__wrap .swiper-container', {
+    loop: true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.blog__wrap .swiper-button-next',
+        prevEl: '.blog__wrap .swiper-button-prev',
+    }
+});
